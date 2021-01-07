a8a893f160ad17e24f5c8862c33fddfe9628549b
joshcarp <joshuacarpeggiani@gmail.com>
2021-01-07 16:05:15 +1100

    Output files

--- main.go
+++ main.go
@@ -2,36 +2,42 @@ package main
 
 import (
 	"bytes"
+	"crypto/md5"
+	"encoding/hex"
 	"flag"
 	"io/ioutil"
+	"mime"
 	"net/http"
 	"net/url"
 	"os"
+	"path"
 	"regexp"
 	"strings"
 
 	"github.com/cheggaaa/pb/v3"
 	"github.com/hashicorp/go-retryablehttp"
 	"github.com/spf13/afero"
-	"github.com/vincent-petithory/dataurl"
 )
 
 var (
-	input = flag.String("input", ".", "input directory to replace links in")
-	r     = flag.String("regex", `!\[.*?\]\((?P<url>.*?)\)`, "regex link to replace")
+	input  = flag.String("input", ".", "input directory to replace links in")
+	r      = flag.String("regex", `!\[.*?\]\((?P<url>.*?)\)`, "regex link to replace")
+	output = flag.String("output", ".", "output directory to put images")
+	prefix = flag.String("prefix", ".", "prefix to prefix links with")
 )
 
 func main() {
+	type replacer struct {
+		old, new string
+		ext      string
+	}
 	flag.Parse()
 	fs := afero.NewOsFs()
 	regex := regexp.MustCompile(*r)
 	prereplace := make(map[string]string)
 	files := make(map[string][]byte)
-	replace := make(map[string]string)
-
-	type replacer struct {
-		old, new string
-	}
+	newSvgFiles := make(map[string][]byte)
+	replace := make(map[string]replacer)
 
 	replaceChan := make(chan replacer)
 	afero.Walk(fs, *input, func(path string, info os.FileInfo, err error) error {
@@ -67,10 +73,16 @@ func main() {
 			}
 			contentType := resp.Header.Get("Content-Type")
 			repl, _ := ioutil.ReadAll(resp.Body)
-			durl := dataurl.New(repl, contentType)
+			mimeType, _ := mime.ExtensionsByType(contentType)
+			var m string
+			for _, e := range mimeType {
+				m = e
+				break
+			}
 			replaceChan <- replacer{
 				old: src,
-				new: durl.String(),
+				new: string(repl),
+				ext: m,
 			}
 		}(src, foundUrl)
 	}
@@ -81,7 +93,7 @@ func main() {
 			if !ok {
 				goto cont
 			}
-			replace[a.old] = a.new
+			replace[a.old] = a
 		default:
 			if done == len(prereplace) {
 				close(replaceChan)
@@ -92,7 +104,9 @@ cont:
 
 	for filename := range files {
 		for src, newurl := range replace {
-			files[filename] = bytes.ReplaceAll(files[filename], []byte(src), []byte(newurl))
+			svgname := HashName(src) + newurl.ext
+			files[filename] = bytes.ReplaceAll(files[filename], []byte(src), []byte(path.Join(*prefix, svgname)))
+			newSvgFiles[svgname] = []byte(newurl.new)
 		}
 	}
 
@@ -101,7 +115,20 @@ cont:
 			panic(err)
 		}
 	}
+	for filename := range newSvgFiles {
+		if err := afero.WriteFile(fs, path.Join(*output, filename), newSvgFiles[filename], os.ModePerm); err != nil {
+			panic(err)
+		}
+	}
 	bar.Finish()
+}
+
+func HashName(s string) string {
+	hash := md5.Sum([]byte(s))
+	src := hash[:]
+	hexPassword := make([]byte, hex.EncodedLen(len(src)))
+	hex.Encode(hexPassword, src)
+	return string(hexPassword)
 }
 
 // RetryHTTPRequest retries the given request
